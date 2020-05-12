2dd623f6784c2b9326469237c84930abe7471c89
justynchen <justynchen@tencent.com>
2020-05-12 15:27:51 +0800

    feat(debug): add standard api for remote debug

--- sdk/debug/index.js
+++ sdk/debug/index.js
@@ -39,6 +39,43 @@ RemoteDebug.prototype.remoteDebug = async function(cliCallback) {
   }
 }
 
+/**
+ * @param option
+ * @param option.stdout {object} writable stream for output log
+ * @param option.logger {function} logger function if you need, console.log would be good
+ */
+RemoteDebug.prototype.standardRemoteDebug = async function(option) {
+  let { logger } = option || {}
+  const { stdout } = option || {}
+  logger = logger || function() {}
+  await this.request.ensureFunctionState()
+  await this.request.startDebugging()
+  await this.request.ensureDebuggingState()
+  const {
+    DebuggingInfo: { Url, Token }
+  } = await this.request.getDebuggingInfo()
+  if (!Url || !Token) {
+    throw Error('Get debugging info error: the remote Url or Token does not exist.')
+  }
+  this.client = new WshubClient({ Url, Token })
+  try {
+    const ret = await this.client.forwardDebug()
+    logger('Debugging listening on ws://127.0.0.1:9222.')
+    logger('For help see https://nodejs.org/en/docs/inspector.')
+    logger(
+      'Please open chorme, and visit chrome://inspect, click [Open dedicated DevTools for Node] to debug your code.'
+    )
+    if (stdout) {
+      logger('--------------------- The realtime log ---------------------')
+      await this.client.forwardLog(stdout)
+    }
+    return ret
+  } catch (e) {
+    logger('Debug init error. Please confirm if the local port 9222 is used')
+    throw e
+  }
+}
+
 RemoteDebug.prototype.stop = async function(cliCallback) {
   try {
     if (this.client) {
