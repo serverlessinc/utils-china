ebe8daf1e4c54248cc5b288c8b073ffa06fff02a
syz <eagle-china@hotmail.com>
2020-08-19 11:01:55 +0800

    change ci ignore node_modules dir

--- sdk/ci/index.js
+++ sdk/ci/index.js
@@ -220,7 +220,9 @@ class Ci {
       '  for file in \\\\`ls \\\\$1\\\\`\r\n' +
       '  do\r\n' +
       "    if [ -d \\\\$1'/'\\\\$file ]; then\r\n" +
-      "      read_dir \\\\$1'/'\\\\$file\r\n" +
+      "      if [ \\\\$file != 'node_modules' ]; then\r\n" +
+      "        read_dir \\\\$1'/'\\\\$file\r\n" +
+      '      fi\r\n' +
       '    else\r\n' +
       "      if [ \\\\$file = 'package.json' ]; then\r\n" +
       '        cd \\\\$1\r\n' +
